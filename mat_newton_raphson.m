48834c33cedd6c53a0004100f68d8bdc662c627f
RegisRambo <regis.rambogarcia@gmail.com>
2017-11-30 19:57:32 -0200

    V 1.0

--- mat_newton_raphson.m
+++ mat_newton_raphson.m
@@ -2,4 +2,26 @@ function xn1=mat_newton_raphson(F,J,xn,N,e1,e2,e3)
 % Metodo de Newton Raphson
 %
 % xn1=mat_newton_raphson(F,J,xn,N,e1,e2,e3)
-
+for i=1:N
+     h=-J(xn)\F(xn);
+     xn1=xn+h;
+     %Criterios de parada
+    if isnan(xn1)
+        xn1=xn;
+        return;
+    end
+    
+    if abs(F(xn1))<=e1 
+        break 
+    end
+    
+    if abs(xn1-xn)<e2 
+        break 
+    end
+    
+    if abs((xn1-xn)/xn1)<e3 
+        break 
+    end
+    
+     xn=xn1;
+end
